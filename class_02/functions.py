6b22ca86d0b478a9d7c4c11824817d6bd781993c
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-10-23 12:51:50 +0100

    class 3

--- class_02/functions.py
+++ class_02/functions.py
@@ -1,8 +1,54 @@
+import random 
+
 a = "raquel quer bolo"
 b = ["raquel", "quer", "bolo"]
 c = a.split(" ")
-print(c)
+#print(c)
 
 d = "money&money&money"
 e = d.split("&")
-print(e) 
+#print(e) 
+
+# FUNCTION DEFINITION =======================================================
+def myFunction(name):
+    print(name)
+    print("Sai daqui {}".format(name)) 
+
+
+#myFunction("lala")
+#myFunction("Bernardo")
+
+# ISOLATE VARIABLES IN FUNCTIONS ============================================
+num1 = 3
+def myOperation(num2): 
+    num1 = 1            # The function isolates its variables.
+    return num1 + num2 * 2 
+
+new_number = myOperation(2)
+#print(new_number)
+#print(num1)
+
+# GLOBAL SCOPE to access global variables  ================================== 
+def myOperation2(num2): 
+    global num1
+    return num1 + num2 * 2 
+
+#print(myOperation2(2))  # Must return 7
+
+# RANDOM ====================================================================
+#r = random.randint(1,9)
+#print(r)
+
+# LAMBDAS ===================================================================
+myOperation3 = lambda num1, num2: num1 + num2
+print(myOperation3(1,2))
+
+# MAP =======================================================================
+# input = 1 2 3 4 
+x = list(map(int, input().split(' ')))     # ["1", "2", "3", "4"]
+
+def myOperation4(x1):
+    return x1*2 
+
+# input = 1 2 3 4 
+x = list(map(myOperation4, input().split(" "))) # ['11', '22', '33', '44']
--- class_02/vectors.py
+++ class_02/vectors.py
@@ -1,5 +1,67 @@
 a = [10,24,33] 
-print(a[0])     # 10
-print(a[-1])    # 33
-print(a[2])     # 33
+#print(a[0])     # 10
+#print(a[-1])    # 33
+#print(a[2])     # 33
 
+
+""" 
+LIST SLICE 
+"""
+
+a = [1,2,3,4,5,6]
+sliced_a = a[2:] # [3,4,5,6]
+sliced_b = a[2:5] # [3,4,5]
+
+"""
+LIST FUNCTIONS 
+"""
+
+# LEN  ======
+list_size = len(a)   # 6
+sliced_c = a[2:list_size]    # [3,4,5,6]
+
+# DEL  =====
+del a[1]    
+#print(a) # [1,3,4,5,6]
+
+raquel_esta = "raquel" in ["raquel", "bernardo", "lalau"]
+#print(raquel_esta)     # True 
+
+one_is_in = 1 in [3,4,5]
+#print(one_is_in)        # False 
+
+# APPEND =====
+miau = ["raquel", "bernardo", "lalau"]
+miau.append("juliane") # ["raquel", "bernardo", "lalau", "juliane"]
+
+# SUM OF LISTS =====
+miau = [miau[0]] + ["juliane"] + miau[1:] # ['raquel', 'juliane', 'bernardo', 'lalau', 'juliane']
+#print(miau)
+
+# SORT ==== 
+# print(miau.sort()) the sort returns nothing, but changes the miau
+c = sorted(miau)    # the c is sorted but not miau.
+#print(c)
+#print(miau)
+
+# SUM ====== 
+sum([1,2,3,4])  # 10 
+
+
+"""
+FOR LOOPS WITH LISTS 
+"""
+
+b = [2,6,10]
+#for i in b: 
+#    print(i)
+
+c = ["raquel", "bernardo", "lalau"]
+#for i in c:
+#    print(i)
+
+d = ["raquel", "bernardo", "lalau"]
+e = list(enumerate(d))  # [(0, 'raquel'), (1, 'bernardo'), (2, 'lalau')] 
+#for i, name in enumerate(d):
+#    print(i)
+#    print(name)
